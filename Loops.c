1474d09ee5d690ccbb8f32dcd60d672608c1b774
Raj <praj11609@gmail.com>
2022-01-29 17:22:50 +0530

    Loops Updated

--- Loops.c
+++ Loops.c
@@ -5,15 +5,21 @@ int main()
     int var = 3;
     int i = 4;
     int j = 0;
+
+    // While loop
     while (j <= var)
     {
         printf("Hi!\n");
         j++;
     }
+
+    // For loop
     for (int i = 0; i <= var; i++)
     {
         printf("Hi!\n");
     }
+
+    // Do-While loop
     do
     {
         printf("Hi!\n");
